3b00cd94f3643f94fc769a96a76bf87cade92dd0
nonakamarcosrc <nonakamarcosrc@hotmail.com>
2022-03-19 21:06:16 -0300

    implementação layout de resposta

--- simulador_de_dado.py
+++ simulador_de_dado.py
@@ -1,24 +1,33 @@
 # Simulador de dado
 # Simular o uso de um dado, gerando um valor de 1 a 6
 import random
+import PySimpleGUI as sg
 
 class SimuladorDeDado:
     def __init__(self):
         self.valor_minimo = 1
         self.valor_maximo = 6
-        self.mensagem = 'Você gostaria de gerar um novo valor para o dado?'
+        # Layout
+        self.layout = [
+            [sg.Text('Jogar o dado?')],
+            [sg.Button('sim'),sg.Button('não')]
+        ]
 
     def Iniciar(self):
-        resposta = input(self.mensagem)
+        # Criar uma janela
+        self.janela = sg.Window('Simulador de dado',layout=self.layout)
+        # Ler os valores da tela
+        self.eventos, self.valores = self.janela.Read()
+        # Fazer alguma coisa com esses valores
         try:
-            if resposta == 'sim' or resposta == 's':
+            if self.eventos == 'sim' or self.eventos == 's':
                 self.GerarValorDoDado()
-            elif resposta == 'não' or resposta == 'n':
-                print('Agradecemos a sua participação!')
+            elif self.eventos == 'não' or self.eventos == 'n':
+                print('Agradecemos a sua participação')
             else:
                 print('Favor digitar sim ou não')
         except:
-            print('Resposta inválida')
+            print('Resposta inválida!')
 
     def GerarValorDoDado(self):
         print(random.randint(self.valor_minimo,self.valor_maximo))
